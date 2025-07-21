4970ab22631725ca037d4cc8ff2f771031646fb1
m-nakamura <kogama3@gmail.co.jp>
2025-07-21 20:19:52 +0900

    rename controller, add codes

--- serverside_challenge_2/challenge/app/controllers/api/v1/plan_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::PlanController < ApplicationController
-  def simulate_all
-    amps = params[:amps]
-    meter_rate = params[:meter_rate]
-    data = { privider_name: "test", plan_name: "test", price: "test", amps: amps, meter_rate: meter_rate}
-    render json: data
-  end
-end
---
+++ serverside_challenge_2/challenge/app/controllers/api/v1/power_supply_plan_controller.rb
@@ -0,0 +1,109 @@
+class Api::V1::PowerSupplyPlanController < ApplicationController
+  def simulate_all
+    amp = params[:amp]
+    meter_rate = params[:meter_rate]
+    validate_result = validate_request(amp, meter_rate)
+    return render json: validate_result if validate_result.present?
+    
+    amp = amp.to_i
+    meter_rate = meter_rate.to_i
+    
+    # 各プランの基本料金を取得
+    plans = PowerSupplyPlan.all
+    result = []
+    for plan in plans do
+      id = plan.id
+      basic_charge_price = basic_charge(id, amp)
+      if basic_charge_price == -1
+        next
+      end
+      meter_charge_price = meter_charge(id, meter_rate)
+      if meter_charge_price == -1
+        next
+      end
+      
+      result << {
+        provider_name: plan.provider_name.to_s,
+        plan_name: plan.plan_name.to_s,
+        price: (basic_charge_price + meter_charge_price).round
+      }
+    end
+    
+    render json: result
+  end
+  
+  private
+
+  def basic_charge(plan_id, amp)
+    basic_charge_record = BasicCharge.where(plan_id: plan_id, amp: amp)
+    if basic_charge_record.count == 1
+      price = basic_charge_record.first.price
+    elsif basic_charge_record.count == 0
+      return -1
+    end
+
+    return price
+  end
+
+  def meter_charge(plan_id, meter_rate)
+    plan = MeterRateCharge.where(
+                                  plan_id: plan_id,
+                                  min_meter_rate: ..meter_rate, 
+                                  max_meter_rate: meter_rate..
+                                )
+    logger.debug "plan.bank?: #{plan.blank?}"
+
+    if plan.blank?
+      maximum_plan = MeterRateCharge.where(
+                                            plan_id: plan_id, 
+                                            max_meter_rate: nil
+                                          )
+      if maximum_plan.first.min_meter_rate <= meter_rate
+        return maximum_plan.first.price * meter_rate.to_f
+      else
+        return -1
+      end
+    else
+
+      return plan.first.price * meter_rate.to_f
+    end
+  end
+
+  def validate_request(amp, meter_rate)
+    message = ""
+    if amp.blank? 
+      message = message + "リクエストパラメータ 'amp' が不足しています。\n" 
+    else
+      message = message + "リクエストパラメータ 'amp' の値が不正です。0以上の整数を指定してください。\n" unless numeric?(amp) && amp.to_i >= 0
+    end
+    if meter_rate.blank?
+      message = message + "リクエストパラメータ 'meter_rate' が不足しています。\n"
+    else
+      message = message + "リクエストパラメータ 'meter_rate' の値が不正です。0以上の整数を指定してください。\n" unless numeric?(meter_rate) && meter_rate.to_i >= 0
+    end
+
+    if message.present?
+      return errro_result(message)
+    end
+
+    return []
+  end
+
+  def numeric?(string)
+    Integer(string)
+    true
+  rescue ArgumentError, TypeError
+    false
+  end
+
+  def errro_result(result)
+    [
+      {
+        error: {
+          code: 400,
+          message: result
+        }
+      }
+    ]
+  end
+end
