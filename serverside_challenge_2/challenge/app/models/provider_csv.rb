78237d904fadae594a3f7a0b4a259b4f6bc9b8ab
m-nakamura <kogama3@gmail.co.jp>
2025-07-24 18:01:20 +0900

    remove csv data and models

--- serverside_challenge_2/challenge/app/models/provider_csv.rb
+++
@@ -1,6 +0,0 @@
-require 'csv' # CSVファイルを扱うためにCSVライブラリをロード
-
-class ProviderCsv < ActiveHash::Base
-  include ActiveHash::Associations
-  self.data = CSV.read('config/data/providers.csv', headers: true).map(&:to_hash)
-end
